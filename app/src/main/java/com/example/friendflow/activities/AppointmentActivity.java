90efdbdc077131be761c9c6fa2f8f8abdc8af7d9
bianka.zieba <bianka.zieba@edu.tbz.ch>
2024-01-19 16:24:29 +0100

    [FEATURE] - Add Linter

--- app/src/main/java/com/example/friendflow/activities/AppointmentActivity.java
+++ app/src/main/java/com/example/friendflow/activities/AppointmentActivity.java
@@ -21,33 +21,26 @@ public class AppointmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
-
         ImageButton backButton = findViewById(R.id.backButton);
         TableRow openPopup = findViewById(R.id.openPopup);
-
         backButton.setOnClickListener(v -> finish());
-
         openPopup.setOnClickListener(view -> showPopup());
-
         EditText tasktitle = findViewById(R.id.tasktitle);
         EditText taskdate = findViewById(R.id.taskdate);
         EditText fromtime = findViewById(R.id.fromtime);
         EditText tilltime = findViewById(R.id.tilltime);
         Button createbutton = findViewById(R.id.createbutton);
-
         createbutton.setOnClickListener(v -> {
             String title = tasktitle.getText().toString();
             String date = taskdate.getText().toString();
             String fromTime = fromtime.getText().toString();
             String tillTime = tilltime.getText().toString();
-
             Intent intent = new Intent(AppointmentActivity.this, CalendarActivity.class);
             intent.putExtra("TITLE", title);
             intent.putExtra("DATE", date);
             intent.putExtra("FROM_TIME", fromTime);
             intent.putExtra("TILL_TIME", tillTime);
             startActivity(intent);
-
             tasktitle.setText("");
             taskdate.setText("");
             fromtime.setText("");
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.1" apply false
+    id ("checkstyle")
+}
+
+checkstyle {
+    toolVersion = ("10.12.7")
+    configFile = rootProject.file("checkstyle.xml")
 }
